54d43cacc2277974b9c3c3ec3a8209fb4bbab0c7
leiner117 <leineralvarador117@gmail.com>
2025-06-25 14:11:44 -0600

    feat: include EfficientNet and MobileNetv2 routers in the main application

--- Web App/backend/main.py
+++ Web App/backend/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.v1 import transcribe
 from core.config import settings
-from api.v1 import images
+from api.v1 import EfficientNet
+from api.v1 import MobileNetv2
 app = FastAPI(title="Audio Transcription API")
 
 # CORS
@@ -16,4 +17,5 @@ app.add_middleware(
 
 # Incluir rutas
 app.include_router(transcribe.router, prefix="/api/v1")
-app.include_router(images.router, prefix="/api/v1")
+app.include_router(EfficientNet.router, prefix="/api/v1")
+app.include_router(MobileNetv2.router, prefix="/api/v1")
