94d8774ca5b90fa3462d3535337ef238e78d3e49
leiner117 <leineralvarador117@gmail.com>
2025-06-25 14:12:38 -0600

    feat: rename predictImageClass to predictImageEfficientNet and add MobileNetv2 prediction function

--- Web App/frontend/src/services/api.ts
+++ Web App/frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1"; // adaptado a la nueva ruta
+const BASE_URL = "http://localhost:8000/api/v1";
 
 export const transcribeAudio = async (audioBlob: Blob): Promise<string> => {
   const formData = new FormData();
@@ -10,13 +10,22 @@ export const transcribeAudio = async (audioBlob: Blob): Promise<string> => {
   return response.data.transcription;
 };
 
-export const predictImageClass = async (imageFile: File | Blob): Promise<{ predicted_class: number; confidence: number }> => {
+export const predictImageEfficientNet = async (imageFile: File | Blob): Promise<{ predicted_class: number; confidence: number }> => {
   const formData = new FormData();
   formData.append("file", imageFile, "image.jpg");
 
   const response = await axios.post(`${BASE_URL}/EfficientNet`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
-
   return response.data;
 };
+
+export const predictImageMobileNet = async (imageFile: File | Blob): Promise<{ predicted_class: number; confidence: number }> => {
+  const formData = new FormData();
+  formData.append("file", imageFile, "image.jpg");
+
+  const response = await axios.post(`${BASE_URL}/MobileNetv2`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return response.data;
+}
